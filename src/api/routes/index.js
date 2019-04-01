3b0e41e4c863d32471d2780925b3691005f0a767
robbieve <robbieeve751994@gmail.com>
2019-04-01 20:10:41 +0700

    added simple restapi request

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const eohdClient = require('../../parser/clients/eodhd');
 
 const router = express.Router();
 
 router.get('/status', (req, res) => res.send('OK'));
+
+router.get('/test', async (req, res) => {
+  const result = await eohdClient.test();
+  res.send(result.data);
+});
 
 module.exports = router;
