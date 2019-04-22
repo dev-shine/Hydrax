78478e7a678191eafb61d63527ff250867a11b05
robbieve <robbieeve751994@gmail.com>
2019-04-22 11:36:33 -0400

    set unique value

--- sql/setup/symbol_codes.sql
+++ sql/setup/symbol_codes.sql
@@ -1,6 +1,6 @@
 CREATE TABLE symbol_codes (
     index bigserial primary key,
-    symbol varchar(64) NOT NULL, -- symbol name or currency pairs with slash (AAPL, BTC/USD)
+    symbol varchar(64) UNIQUE NOT NULL, -- symbol name or currency pairs with slash (AAPL, BTC/USD)
     type varchar(64) NOT NULL, -- data type (e.g crypto_currency, FX, Stock, Indices, ETFs)
     exchange varchar(64) NOT NULL, -- data source name with uppercase letters (e.g. NASDAQ)
     service varchar(64) NOT NULL, -- data provider service name with uppercase letters (e.g. EOD)
